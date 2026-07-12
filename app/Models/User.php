19322e6ff05fe2ce341d4ab0da4aaed462d517c4
Romulo <romulosantos.62014@gmail.com>
2026-07-12 15:49:11 -0300

    feat: implement posts CRUD endpoints

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,28 +2,28 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable, HasApiTokens;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
     protected function casts(): array
     {
         return [
@@ -36,5 +36,4 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class, 'user_id');
     }
-
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CommentController;
+//use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
-use Illuminate\Session\Store;
+//use Illuminate\Session\Store;
 use Illuminate\Support\Facades\Route;
 
 Route::post('auth/register', [AuthController::class, 'register']);
@@ -13,13 +13,13 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('auth/logout', [AuthController::class, 'logout']);
 
-    /*
+    
     Route::post('posts', [PostController::class, 'store']);
-    Route::patch('posts/{id}', [PostController::class, 'update']);
-    Route::delete('posts/{id}', [PostController::class, 'destroy']);
+    Route::patch('posts/{post}', [PostController::class, 'update']);
+    Route::delete('posts/{post}', [PostController::class, 'destroy']);
     Route::get('posts', [PostController::class, 'index']);
-    Route::get('posts/{id}', [PostController::class, 'show']);
-
+    Route::get('posts/{post}', [PostController::class, 'show']);
+/*
     Route::post('posts/{id}/comments', [CommentController::class,'store']);
     Route::get('comments/{id}', [CommentController::class,'show']);
 */
